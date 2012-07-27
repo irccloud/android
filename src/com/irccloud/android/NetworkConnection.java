f15ebbe09f8d37f71efecd3af9ae5145e0bd7894
Sam Steele <sam@last.fm>
2012-07-26 19:05:57 -0700

    Request and parse the gzip-encoded backlog

--- src/com/irccloud/android/NetworkConnection.java
+++ src/com/irccloud/android/NetworkConnection.java
@@ -1,15 +1,27 @@
 package com.irccloud.android;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URI;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.zip.GZIPInputStream;
+
+import javax.net.ssl.HttpsURLConnection;
+
 import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.json.JSONTokener;
 
+import android.os.AsyncTask;
 import android.os.Handler;
+import android.os.Looper;
 import android.os.Message;
 import android.util.Log;
 import com.codebutler.android_websockets.WebSocketClient;
@@ -26,6 +38,7 @@ public class NetworkConnection {
 	WebSocketClient client = null;
 	UserInfo userInfo = null;
 	ArrayList<Handler> handlers = null;
+	String session = null;
 	
 	public static final int EVENT_CONNECTIVITY = 0;
 	public static final int EVENT_USERINFO = 1;
@@ -47,8 +60,10 @@ public class NetworkConnection {
 	}
 	
 	public void connect(String sk) {
+		session = sk;
+		
 		List<BasicNameValuePair> extraHeaders = Arrays.asList(
-		    new BasicNameValuePair("Cookie", "session="+sk)
+		    new BasicNameValuePair("Cookie", "session="+session)
 		);
 
 		client = new WebSocketClient(URI.create("wss://irccloud.com"), new WebSocketClient.Listener() {
@@ -61,8 +76,15 @@ public class NetworkConnection {
 
 		    @Override
 		    public void onMessage(String message) {
-		    	if(message.length() > 0)
-		    		parse_message(message);
+		    	if(message.length() > 0) {
+					try {
+			    		parse_object(new JSONObject(message));
+					} catch (JSONException e) {
+						Log.e(TAG, "Unable to parse: " + message);
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+		    	}
 		    }
 
 		    @Override
@@ -88,24 +110,73 @@ public class NetworkConnection {
 		client.connect();
 	}
 	
-	private void parse_message(String message) {
-		try {
-			JSONObject object = (JSONObject)new JSONTokener(message).nextValue();
-			String type = object.getString("type");
-			if(type != null && type.length() > 0) {
-				Log.d(TAG, "New message! Type: " + type);
-				if(type.equalsIgnoreCase("stat_user")) {
-					userInfo = new UserInfo(object);
-					notifyHandlers(EVENT_USERINFO, userInfo);
+	private void parse_object(JSONObject object) throws JSONException {
+		String type = object.getString("type");
+		if(type != null && type.length() > 0) {
+			Log.d(TAG, "New message! Type: " + type);
+			if(type.equalsIgnoreCase("stat_user")) {
+				userInfo = new UserInfo(object);
+				notifyHandlers(EVENT_USERINFO, userInfo);
+			} else if(type.equalsIgnoreCase("oob_include")) {
+				try {
+					Looper.prepare();
+					new OOBIncludeTask().execute(new URL("https://irccloud.com" + object.getString("url")));
+				} catch (MalformedURLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
 				}
 			}
-		} catch (JSONException e) {
-			Log.e(TAG, "Unable to parse: " + message);
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 		}
 	}
 	
+	private String doGet(URL url) throws IOException {
+		Log.d(TAG, "Requesting: " + url);
+		
+		HttpURLConnection conn = null;
+
+        if (url.getProtocol().toLowerCase().equals("https")) {
+            HttpsURLConnection https = (HttpsURLConnection) url.openConnection();
+            conn = https;
+        } else {
+        	conn = (HttpURLConnection) url.openConnection();
+        }
+		conn.setRequestMethod("GET");
+		conn.setRequestProperty("connection", "close");
+		conn.setRequestProperty("Cookie", "session="+session);
+		conn.setRequestProperty("Accept", "application/json");
+		conn.setRequestProperty("Content-type", "application/json");
+		conn.setRequestProperty("Accept-Encoding", "gzip");
+		BufferedReader reader = null;
+		String response = "";
+		conn.connect();
+		try {
+			if(conn.getInputStream() != null) {
+				if(conn.getContentEncoding().equalsIgnoreCase("gzip"))
+					reader = new BufferedReader(new InputStreamReader(new GZIPInputStream(conn.getInputStream())), 512);
+				else
+					reader = new BufferedReader(new InputStreamReader(conn.getInputStream()), 512);
+			}
+		} catch (IOException e) {
+			if(conn.getErrorStream() != null)
+				reader = new BufferedReader(new InputStreamReader(conn.getErrorStream()), 512);
+		}
+
+		if(reader != null) {
+			response = toString(reader);
+			reader.close();
+		}
+		return response;
+	}
+
+	private static String toString(BufferedReader reader) throws IOException {
+		StringBuilder sb = new StringBuilder();
+		String line;
+		while ((line = reader.readLine()) != null) {
+			sb.append(line).append('\n');
+		}
+		return sb.toString();
+	}
+
 	public void addHandler(Handler handler) {
 		if(handlers == null)
 			handlers = new ArrayList<Handler>();
@@ -161,6 +232,36 @@ public class NetworkConnection {
 			limit_zombiehours = limits.getLong("zombiehours");
 			limit_download_logs = limits.getBoolean("download_logs");
 			limit_maxhistorydays = limits.getLong("maxhistorydays");
+		}
+	}
+	
+	private class OOBIncludeTask extends AsyncTask<URL, Void, Boolean> {
+		String json = null;
+		
+		@Override
+		protected Boolean doInBackground(URL... url) {
+			try {
+				json = doGet(url[0]);
+				return true;
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return false;
+		}
+
+		@Override
+		public void onPostExecute(Boolean result) {
+			if(result) {
+				try {
+					JSONArray a = new JSONArray(json);
+					for(int i = 0; i < a.length(); i++)
+						parse_object(a.getJSONObject(i));
+				} catch (JSONException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
 		}
 	}
 }
