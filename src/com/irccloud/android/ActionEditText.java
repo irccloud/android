2f93ee6403e43213a2861499266d53c686c714e5
Sam Steele <sam.steele@gmail.com>
2012-11-20 09:26:03 -0800

    Also override the NAVIGATE_PREVIOUS and NAVIGATE_NEXT flags in our edittext subclass

--- src/com/irccloud/android/ActionEditText.java
+++ src/com/irccloud/android/ActionEditText.java
@@ -7,6 +7,7 @@ import android.view.inputmethod.InputConnection;
 import android.widget.EditText;
 
 // An EditText that lets you use actions ("Done", "Go", etc.) on multi-line edits.
+// From: http://stackoverflow.com/a/12570003/1406639
 public class ActionEditText extends EditText
 {
     public ActionEditText(Context context)
@@ -29,6 +30,8 @@ public class ActionEditText extends EditText
     {
         InputConnection conn = super.onCreateInputConnection(outAttrs);
         outAttrs.imeOptions &= ~EditorInfo.IME_FLAG_NO_ENTER_ACTION;
+        outAttrs.imeOptions &= ~EditorInfo.IME_FLAG_NAVIGATE_PREVIOUS;
+        outAttrs.imeOptions &= ~EditorInfo.IME_FLAG_NAVIGATE_NEXT;
         return conn;
     }
 }
