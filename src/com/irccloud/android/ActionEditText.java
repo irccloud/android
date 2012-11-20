681b2cb3b140337e55b687a28abdf39504eb586e
Sam Steele <sam.steele@gmail.com>
2012-11-20 08:38:44 -0800

    Subclass EditText to prevent it from overriding the send button with an enter button in multiline mode

---
+++ src/com/irccloud/android/ActionEditText.java
@@ -0,0 +1,34 @@
+package com.irccloud.android;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputConnection;
+import android.widget.EditText;
+
+// An EditText that lets you use actions ("Done", "Go", etc.) on multi-line edits.
+public class ActionEditText extends EditText
+{
+    public ActionEditText(Context context)
+    {
+        super(context);
+    }
+
+    public ActionEditText(Context context, AttributeSet attrs)
+    {
+        super(context, attrs);
+    }
+
+    public ActionEditText(Context context, AttributeSet attrs, int defStyle)
+    {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    public InputConnection onCreateInputConnection(EditorInfo outAttrs)
+    {
+        InputConnection conn = super.onCreateInputConnection(outAttrs);
+        outAttrs.imeOptions &= ~EditorInfo.IME_FLAG_NO_ENTER_ACTION;
+        return conn;
+    }
+}
