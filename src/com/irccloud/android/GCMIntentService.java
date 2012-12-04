492761d9a150fb073ffb274ba9aba14bd7511fb3
Sam Steele <sam.steele@gmail.com>
2012-12-04 08:08:27 -0800

    handle heartbeat_echo GCM messages

--- src/com/irccloud/android/GCMIntentService.java
+++ src/com/irccloud/android/GCMIntentService.java
@@ -1,7 +1,9 @@
 package com.irccloud.android;
 
+import java.util.Iterator;
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.Map.Entry;
 
 import org.json.JSONObject;
 
@@ -13,6 +15,9 @@ import android.util.Log;
 
 import com.google.android.gcm.GCMBaseIntentService;
 import com.google.android.gcm.GCMRegistrar;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 public class GCMIntentService extends GCMBaseIntentService {
 
@@ -30,35 +35,53 @@ public class GCMIntentService extends GCMBaseIntentService {
 		if(intent != null && intent.getExtras() != null) {
 	    	Log.d("IRCCloud", "GCM K/V pairs: " + intent.getExtras().toString());
 	    	try {
-		    	int cid = Integer.valueOf(intent.getStringExtra("cid"));
-		    	int bid = Integer.valueOf(intent.getStringExtra("bid"));
-		    	long eid = Long.valueOf(intent.getStringExtra("eid"));
-		    	if(Notifications.getInstance().getNotification(eid) != null) {
-		    		Log.d("IRCCloud", "A notification for this event already exists in the db, ignoring");
-		    		return;
-		    	}
-		    	String from = intent.getStringExtra("from_nick");
-		    	String msg = ColorFormatter.html_to_spanned(ColorFormatter.irc_to_html(intent.getStringExtra("msg"))).toString();
-		    	String chan = intent.getStringExtra("chan");
-		    	if(chan == null)
-		    		chan = "";
 		    	String type = intent.getStringExtra("type");
-		    	String buffer_type = intent.getStringExtra("buffer_type");
-		    	String server_name = intent.getStringExtra("server_name");
-		    	if(server_name == null || server_name.length() == 0)
-		    		server_name = intent.getStringExtra("server_hostname");
-		    	
-		    	String network = Notifications.getInstance().getNetwork(cid);
-		    	if(network == null)
-		    		Notifications.getInstance().addNetwork(cid, server_name);
-		    	
-		    	Notifications.getInstance().deleteNotification(cid, bid, eid);
-		    	Notifications.getInstance().addNotification(cid, bid, eid, from, msg, chan, buffer_type, type);
-		    	
-				if(buffer_type.equals("channel"))
-					Notifications.getInstance().showNotifications(chan + ": <" + from + "> " + msg);
-				else
-					Notifications.getInstance().showNotifications(from + ": " + msg);
+		    	if(type.equalsIgnoreCase("heartbeat_echo")) {
+		    		JsonParser parser = new JsonParser();
+		    		JsonObject seenEids = parser.parse(intent.getStringExtra("seenEids")).getAsJsonObject();
+					Iterator<Entry<String, JsonElement>> i = seenEids.entrySet().iterator();
+					while(i.hasNext()) {
+						Entry<String, JsonElement> entry = i.next();
+						JsonObject eids = entry.getValue().getAsJsonObject();
+						Iterator<Entry<String, JsonElement>> j = eids.entrySet().iterator();
+						while(j.hasNext()) {
+							Entry<String, JsonElement> eidentry = j.next();
+							String bid = eidentry.getKey();
+							long eid = eidentry.getValue().getAsLong();
+							Notifications.getInstance().deleteOldNotifications(Integer.valueOf(bid), eid);
+							Notifications.getInstance().updateLastSeenEid(Integer.valueOf(bid), eid);
+						}
+					}
+		    	} else {
+			    	int cid = Integer.valueOf(intent.getStringExtra("cid"));
+			    	int bid = Integer.valueOf(intent.getStringExtra("bid"));
+			    	long eid = Long.valueOf(intent.getStringExtra("eid"));
+			    	if(Notifications.getInstance().getNotification(eid) != null) {
+			    		Log.d("IRCCloud", "A notification for this event already exists in the db, ignoring");
+			    		return;
+			    	}
+			    	String from = intent.getStringExtra("from_nick");
+			    	String msg = ColorFormatter.html_to_spanned(ColorFormatter.irc_to_html(intent.getStringExtra("msg"))).toString();
+			    	String chan = intent.getStringExtra("chan");
+			    	if(chan == null)
+			    		chan = "";
+			    	String buffer_type = intent.getStringExtra("buffer_type");
+			    	String server_name = intent.getStringExtra("server_name");
+			    	if(server_name == null || server_name.length() == 0)
+			    		server_name = intent.getStringExtra("server_hostname");
+			    	
+			    	String network = Notifications.getInstance().getNetwork(cid);
+			    	if(network == null)
+			    		Notifications.getInstance().addNetwork(cid, server_name);
+			    	
+			    	Notifications.getInstance().deleteNotification(cid, bid, eid);
+			    	Notifications.getInstance().addNotification(cid, bid, eid, from, msg, chan, buffer_type, type);
+			    	
+					if(buffer_type.equals("channel"))
+						Notifications.getInstance().showNotifications(chan + ": <" + from + "> " + msg);
+					else
+						Notifications.getInstance().showNotifications(from + ": " + msg);
+		    	}
 	    	} catch (Exception e) {
 	    		e.printStackTrace();
 	    		Log.w("IRCCloud", "Unable to parse GCM message");
