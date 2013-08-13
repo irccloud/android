fc5b1249ea1d913f9208b45fc5c167e32f53a8b2
Sam Steele <sam.steele@gmail.com>
2013-08-13 08:34:55 -0700

    Remove ActionBarSherlock from the licenses list

--- src/com/irccloud/android/PreferencesActivity.java
+++ src/com/irccloud/android/PreferencesActivity.java
@@ -384,21 +384,6 @@ public class PreferencesActivity extends PreferenceActivity {
                     "You should have received a copy of the GNU General Public License\n" +
                     "along with this program.  If not, see &lt;http://www.gnu.org/licenses/&gt;.\n" +
                     "\n\n" +
-                    "ActionBarSherlock\n" +
-                    "Copyright (C) 2010 The Android Open Source Project\n" +
-                    "\n" +
-                    "Licensed under the Apache License, Version 2.0 (the \"License\");\n" +
-                    "you may not use this file except in compliance with the License.\n" +
-                    "You may obtain a copy of the License at\n" +
-                    "\n" +
-                    "http://www.apache.org/licenses/LICENSE-2.0\n" +
-                    "\n" +
-                    "Unless required by applicable law or agreed to in writing, software\n" +
-                    "distributed under the License is distributed on an \"AS IS\" BASIS,\n" +
-                    "WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\n" +
-                    "See the License for the specific language governing permissions and\n" +
-                    "limitations under the License.\n" +
-                    "\n\n" +
                     "HybiParser.java: draft-ietf-hybi-thewebsocketprotocol-13 parser\n" +
                     "\n" +
                     "Based on code from the faye project.\n" +
