cae7ac1d98dbf697bbeb2876f575ee64d36d30cb
Sam Steele <sam.steele@gmail.com>
2014-11-07 10:16:44 -0800

    Tweak the IME settings so SwiftKey will show the emoji button

--- src/com/irccloud/android/ActionEditText.java
+++ src/com/irccloud/android/ActionEditText.java
@@ -53,11 +53,13 @@ public class ActionEditText extends EditText
     public InputConnection onCreateInputConnection(EditorInfo outAttrs)
     {
         InputConnection conn = super.onCreateInputConnection(outAttrs);
-        outAttrs.imeOptions &= ~EditorInfo.IME_FLAG_NO_ENTER_ACTION;
         outAttrs.imeOptions &= ~EditorInfo.IME_FLAG_NAVIGATE_PREVIOUS;
         outAttrs.imeOptions &= ~EditorInfo.IME_FLAG_NAVIGATE_NEXT;
         if(PreferenceManager.getDefaultSharedPreferences(IRCCloudApplication.getInstance().getApplicationContext()).getBoolean("kb_send", true)) {
+            outAttrs.imeOptions &= ~EditorInfo.IME_FLAG_NO_ENTER_ACTION;
             outAttrs.inputType = EditorInfo.TYPE_CLASS_TEXT|EditorInfo.TYPE_TEXT_FLAG_AUTO_CORRECT;
+        } else {
+            outAttrs.imeOptions |= EditorInfo.IME_FLAG_NO_ENTER_ACTION;
         }
         if(PreferenceManager.getDefaultSharedPreferences(IRCCloudApplication.getInstance().getApplicationContext()).getBoolean("kb_caps", true)) {
             outAttrs.inputType |= EditorInfo.TYPE_TEXT_FLAG_CAP_SENTENCES;
