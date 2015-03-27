b6ff622d2256dab3fe0b961b4372b3e7fd5d8fcf
Sam Steele <sam.steele@gmail.com>
2015-03-27 10:44:08 -0700

    Only use imgur for image uploads on Android 2.3 as webkit can't load images from our CDN until Android 4.0

--- src/com/irccloud/android/NetworkConnection.java
+++ src/com/irccloud/android/NetworkConnection.java
@@ -2484,7 +2484,7 @@ public class NetworkConnection {
 	}
 
     public boolean uploadsAvailable() {
-        return userInfo != null && !userInfo.uploads_disabled;
+        return Build.VERSION.SDK_INT > Build.VERSION_CODES.HONEYCOMB && userInfo != null && !userInfo.uploads_disabled;
     }
 
 	public boolean isVisible() {
