4eb61a64a31e39b91e96eaa296a97e6d060b3b29
Sam Steele <sam.steele@gmail.com>
2015-09-21 12:26:42 -0700

    generated RGB color palettes for the dark themes

---
+++ build-themes.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+python dark-theme-styles.py ash 210 0
+python dark-theme-styles.py dusk 210 0.55
+python dark-theme-styles.py emerald 110 0.55
+python dark-theme-styles.py orchid 314 0.55
+python dark-theme-styles.py rust 0 0.55
+python dark-theme-styles.py sand 45 0.55
+python dark-theme-styles.py tropic 180 0.55
+
