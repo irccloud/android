1bd2a67046d7a2247bc5709f3e83eebbad841c05
Sam Steele <sam.steele@gmail.com>
2016-01-13 15:28:30 -0800

    TLS_DHE_DSS_WITH_AES_128_CBC_SHA isn't available on Marshmallow

--- src/com/irccloud/android/NetworkConnection.java
+++ src/com/irccloud/android/NetworkConnection.java
@@ -260,7 +260,6 @@ public class NetworkConnection {
                 "TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA",
                 "TLS_DHE_RSA_WITH_AES_128_CBC_SHA",
                 "TLS_DHE_RSA_WITH_AES_256_CBC_SHA",
-                "TLS_DHE_DSS_WITH_AES_128_CBC_SHA",
                 "TLS_ECDHE_RSA_WITH_RC4_128_SHA",
                 "TLS_ECDHE_ECDSA_WITH_RC4_128_SHA",
                 "TLS_RSA_WITH_AES_128_CBC_SHA",
