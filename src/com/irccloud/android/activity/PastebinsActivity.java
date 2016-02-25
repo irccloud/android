4a45b85ba828eb225f4eff3c6323220145c32ece
Sam Steele <sam.steele@gmail.com>
2016-02-25 14:56:04 -0500

    Update to Android Support Library v23.2

--- src/com/irccloud/android/activity/PastebinsActivity.java
+++ src/com/irccloud/android/activity/PastebinsActivity.java
@@ -235,7 +235,7 @@ public class PastebinsActivity extends BaseActivity {
 
         if(getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-            getSupportActionBar().setHomeAsUpIndicator(R.drawable.abc_ic_ab_back_mtrl_am_alpha);
+            getSupportActionBar().setHomeAsUpIndicator(android.support.v7.appcompat.R.drawable.abc_ic_ab_back_material);
             getSupportActionBar().setElevation(0);
         }
 
