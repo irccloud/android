295c0b6dacb94e05bd230a951a520ee66579c6b9
Sam Steele <sam@irccloud.com>
2016-07-25 11:58:05 -0400

    Set FLAG_ACTIVITY_NEW_TASK when launching a 3rd party app from a URL

--- src/com/irccloud/android/IRCCloudLinkMovementMethod.java
+++ src/com/irccloud/android/IRCCloudLinkMovementMethod.java
@@ -79,6 +79,7 @@ public class IRCCloudLinkMovementMethod extends LinkMovementMethod {
                         }
                     } else {
                         Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                         intent.putExtra(Browser.EXTRA_APPLICATION_ID, context.getPackageName());
                         if(Build.VERSION.SDK_INT >= 22)
                             intent.putExtra(Intent.EXTRA_REFERRER, Uri.parse(Intent.URI_ANDROID_APP_SCHEME + "//" + context.getPackageName()));
