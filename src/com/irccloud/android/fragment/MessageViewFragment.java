ff71d8f731a3f6216dccc91e8e1e73ebf7e734e7
Sam Steele <sam.steele@gmail.com>
2017-12-06 08:10:30 -0500

    Fix duplicate messages when PMing yourself

--- src/com/irccloud/android/fragment/MessageViewFragment.java
+++ src/com/irccloud/android/fragment/MessageViewFragment.java
@@ -291,7 +291,7 @@ public class MessageViewFragment extends ListFragment implements NetworkConnecti
         public void clearPending() {
             synchronized (data) {
                 for (int i = 0; i < data.size(); i++) {
-                    if (data.get(i).reqid != -1 && data.get(i).color == colorScheme.timestampColor) {
+                    if (data.get(i).pending) {
                         data.remove(i);
                         i--;
                     }
