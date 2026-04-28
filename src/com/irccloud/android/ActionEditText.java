638b84abb5145f8e28f27b02e38e0fbd3acc8c7d
Sam Steele <sam.steele@gmail.com>
2026-04-28 10:18:51 -0500

    always show the send button on the keyboard

--- src/com/irccloud/android/ActionEditText.java
+++ src/com/irccloud/android/ActionEditText.java
@@ -57,13 +57,8 @@ public class ActionEditText extends IRCEditText {
 
         outAttrs.imeOptions &= ~EditorInfo.IME_FLAG_NAVIGATE_PREVIOUS;
         outAttrs.imeOptions &= ~EditorInfo.IME_FLAG_NAVIGATE_NEXT;
-        if (PreferenceManager.getDefaultSharedPreferences(IRCCloudApplication.getInstance().getApplicationContext()).getBoolean("kb_send", false)) {
-            outAttrs.imeOptions &= ~EditorInfo.IME_FLAG_NO_ENTER_ACTION;
-            outAttrs.inputType = EditorInfo.TYPE_CLASS_TEXT | EditorInfo.TYPE_TEXT_FLAG_AUTO_CORRECT;
-        } else {
-            outAttrs.inputType = EditorInfo.TYPE_CLASS_TEXT | EditorInfo.TYPE_TEXT_FLAG_AUTO_CORRECT | EditorInfo.TYPE_TEXT_VARIATION_SHORT_MESSAGE;
-            outAttrs.imeOptions |= EditorInfo.IME_FLAG_NO_ENTER_ACTION;
-        }
+        outAttrs.imeOptions &= ~EditorInfo.IME_FLAG_NO_ENTER_ACTION;
+        outAttrs.inputType = EditorInfo.TYPE_CLASS_TEXT | EditorInfo.TYPE_TEXT_FLAG_AUTO_CORRECT;
         if (PreferenceManager.getDefaultSharedPreferences(IRCCloudApplication.getInstance().getApplicationContext()).getBoolean("kb_caps", true)) {
             outAttrs.inputType |= EditorInfo.TYPE_TEXT_FLAG_CAP_SENTENCES;
         } else {
